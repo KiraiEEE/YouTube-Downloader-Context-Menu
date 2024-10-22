324320789de277563ccf8533da214ca800530f73
Akram Ben Fekih <54278089+KiraiEEE@users.noreply.github.com>
2024-10-22 16:36:40 +0100

    Add files via upload

---
+++ YT-Downloader.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+:: Get the link from the clipboard
+for /f "tokens=*" %%i in ('powershell -command "Get-Clipboard"') do set link=%%i
+
+:: Set current directory to where the context menu was opened
+cd /d %~dp0
+
+:: Run yt-dlp with the clipboard link and download to the current location
+yt-dlp %link%
