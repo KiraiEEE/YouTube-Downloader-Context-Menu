77f7e9c70df66e75b34171a3f84a23a1a9458c92
KiraiEEE <54278089+KiraiEEE@users.noreply.github.com>
2024-11-30 15:00:46 +0100

    Fixed to save the media in Current right clicked location UwU

--- YT-Downloader.bat
+++ YT-Downloader.bat
@@ -1,10 +1,6 @@
 @echo off
 setlocal
-:: Get the link from the clipboard
+
 for /f "tokens=*" %%i in ('powershell -command "Get-Clipboard"') do set link=%%i
-
-:: Set current directory to where the context menu was opened
-cd /d %~dp0
-
-:: Run yt-dlp with the clipboard link and download to the current location
-yt-dlp -f "bv*+ba/b" %link%
+cd /d "%CD%"
+yt-dlp -f best "%link%"
